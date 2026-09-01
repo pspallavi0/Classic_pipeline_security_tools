5aa8162e3fd90c63ad2a3b365346d046b834476f
pspallavi0 <pspallavi0@gmail.com>
2026-09-01 21:08:24 +0530

    Refactor provider.tf for Terraform configuration

--- environment/preprod/provider.tf
+++ environment/preprod/provider.tf
@@ -1,18 +1,21 @@
-terraform{
-    required_providers{
-        azurerm={
-            source="hashicorp/azurerm"
-            version="4.78.0"
-        }
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.78.0"
     }
-    backend"azurerm"{
-        resource_group_name="vistara-rg"
-        storage_account_name="vistarastg"
-        container_name="vistaracontainer"
-        key="terraform.tfstate"
-    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "vistara-rg"
+    storage_account_name = "vistarastg"
+    container_name       = "vistaracontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
-provider"azurerm"{
-    features{}
+provider "azurerm" {
+  features {}
 }
